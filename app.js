c3cedc82398fa9efb4ad2e3a7059e299d1b6b1b5
meilixiaozhang <redpangci@gmail.com>
2020-04-13 21:43:47 +1000

    Level 2 - Database Encryption

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose")
+const encrypt = require("mongoose-encryption")
 
 const app = express();
 
@@ -19,10 +20,13 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
   useFindAndModify: false
 })
 
-const userSchema = {
+const userSchema = new mongoose.Schema ({
   email: String,
   password: String
-}
+})
+
+const secret = "Thisisourlittlesecret";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
