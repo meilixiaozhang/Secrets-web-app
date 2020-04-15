2afa8c9f9c422563c7a90805be1601b9344d88b3
meilixiaozhang <redpangci@gmail.com>
2020-04-15 19:12:29 +1000

    update app.js

--- app.js
+++ app.js
@@ -193,11 +193,6 @@ app.post("/submit", function(req, res) {
   })
 })
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-
 app.listen(port, function() {
   console.log("Server started successfully");
 });
